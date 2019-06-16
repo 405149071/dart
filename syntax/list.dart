4a0dc7446f7bc080c4a78654ac336a7a35f18884
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-06-16 22:55:12 +0800

    list

---
+++ syntax/list.dart
@@ -0,0 +1,27 @@
+main(){
+
+// 3种方式
+// 1
+
+var l1 = ["1",2,3];
+print(l1);
+print(l1[0]);
+
+
+// 2
+var l2 = new List();
+l2.add("张三");
+l2.add("李四");
+l2.add("王五");
+
+print(l2);
+
+
+// 3
+var l3 = new List<String>();
+l3.add("北京");
+l3.add("222");
+l3.add("天津");
+print(l3);
+
+}
