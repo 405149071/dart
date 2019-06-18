1d116b59ddd501ca5a06d9116ca1e2220c31f6e7
wuzhizhong@duotel.cn <wuzhizhong@duotel.cn>
2019-06-19 07:47:40 +0800

    构造函数，命名构造函数，get set，

--- syntax/object.dart
+++ syntax/object.dart
@@ -1,12 +1,26 @@
 class Person {
+  // dart 没有public ,protect,private的限定，默认都是共有属性和方法， _name 就是私有，但是类必须独立一个文件才行
   String name;
   int age;
 
+  //默认构造函数
   Person(this.name, this.age);
-
+  //命名构造函数
+  Person.nameha(String name){
+    this.name = name;
+  }
+  // 常规方法
   printInfo() {
     print("name = $name , age = $age");
   }
+  // get set 计算属性
+  get prtInfo{
+    print("计算属性");
+  }
+  set prtInfo(name){
+    this.name = name;
+  }
+
 }
 
 class Web extends Person{
@@ -15,6 +29,19 @@ class Web extends Person{
   
   }
 
+}
+
+// 初始化，发生在构造函数之前,很少用
+class Animal {
+  String name;
+  int age;
+  Animal(String name):name="dog",age=3{
+    this.name = name;
+  }
+  // 或 Animal():name="dog",age=3;
+  printInfo(){
+    print("$name ======= $age");
+  }
 }
 
 main() {
@@ -34,5 +61,16 @@ main() {
 
   Web web = new Web("web1", 100);
    web.printInfo();
+
+   Person p2=Person.nameha("name");
+   p2 = new Person("name1111", 22);
+   p2.prtInfo = "hohohola";// 计算属性 set
+   p2.prtInfo; // 计算属性 get
+   p2.printInfo();
+   
+
+   // 类的初始化 （很少用）
+   Animal dog = new Animal("cat");
+   dog.printInfo();
 
 }
